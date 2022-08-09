09cf2ab6c7a723426a345797bbe40aa38d611247
rafftod <ramaluto@gmail.com>
2022-08-09 16:30:20 +0200

    Check proximity sensors

--- src/ARGoS3_AutoMoDe/src/modules/AutoMoDeBehaviourStop.cpp
+++ src/ARGoS3_AutoMoDe/src/modules/AutoMoDeBehaviourStop.cpp
@@ -53,7 +53,8 @@ namespace argos
 
 	void AutoMoDeBehaviourStop::ControlStep()
 	{
-		std::cout << m_pcRobotDAO->GetGroundReading() << std::endl;
+		LOG << m_pcRobotDAO->GetProximityReading().Value << std::endl;
+		LOG << m_pcRobotDAO->GetProximityReading().Angle << std::endl;
 		m_pcRobotDAO->SetWheelsVelocity(0, 0);
 		m_bLocked = false;
 	}
