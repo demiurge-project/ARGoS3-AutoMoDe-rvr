193aac13132bb5d8ce92f26a582afabed48d15ba
rafftod <ramaluto@gmail.com>
2022-08-13 15:39:23 +0200

    rep

--- src/ARGoS3_AutoMoDe/src/modules/AutoMoDeBehaviourRepulsion.cpp
+++ src/ARGoS3_AutoMoDe/src/modules/AutoMoDeBehaviourRepulsion.cpp
@@ -64,7 +64,7 @@ namespace argos
 		}
 
 		sProxVector = CVector2(m_pcRobotDAO->GetProximityReading().Value, m_pcRobotDAO->GetProximityReading().Angle);
-		sResultVector = -m_unRepulsionParameter * sRabVector - 3 * sProxVector;
+		sResultVector = -m_unRepulsionParameter * sRabVector - 5 * sProxVector;
 		std::cout << "Attraction vector " << sRabVector.Length() << " " << sRabVector.Angle() << std::endl;
 		std::cout << "Prox vector " << sProxVector.Length() << " " << sProxVector.Angle() << std::endl;
 		std::cout << "Result vector " << sResultVector.Length() << " " << sResultVector.Angle() << std::endl;
