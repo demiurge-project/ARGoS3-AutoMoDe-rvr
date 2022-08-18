1f505eec77e4372ac3d8c26af8804e7a52f7bcb8
rafftod <ramaluto@gmail.com>
2022-08-18 14:10:45 +0200

    aa

--- src/ARGoS3_AutoMoDe/src/modules/AutoMoDeBehaviourExploration.cpp
+++ src/ARGoS3_AutoMoDe/src/modules/AutoMoDeBehaviourExploration.cpp
@@ -61,6 +61,7 @@ namespace argos
 		std::map<std::string, Real>::iterator it = m_mapParameters.find("rwm");
 		if (it != m_mapParameters.end())
 		{
+			std::cout << it->second * fTimeStepFactor << std::endl;
 			m_cRandomStepsRange.SetMin(fTimeStepFactor);
 			m_cRandomStepsRange.SetMax(it->second * fTimeStepFactor);
 		}
